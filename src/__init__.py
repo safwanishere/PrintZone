e4287487234d239ef75362a383b5944d02f6b541
Mohammed Safwan <mohammedsafwan261205@gmail.com>
2025-12-04 23:00:23 +0530

    filesystem session bug

--- src/__init__.py
+++ src/__init__.py
@@ -5,9 +5,6 @@ def createApp():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "hadfhdfadfj"
 
-    app.config["SESSION_PERMANENT"] = False
-    app.config["SESSION_TYPE"] = "filesystem"
-    Session(app)
 
     app.config['MAX_CONTENT_LENGTH'] = 30 * 1024 * 1024
     app.config['UPLOAD_EXTENSIONS'] = ['.pdf', '.png', '.jpg', '.jpeg']
